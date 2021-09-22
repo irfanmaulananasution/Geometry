6af0dbe9818bb5c85ab3c8542a7827fe11b21760
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2021-09-22 09:16:10 +0700

    Line give 0 length for all zero input

---
+++ src/test/java/com/geometry/LineTest.java
@@ -0,0 +1,15 @@
+package com.geometry;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class LineTest {
+
+    @Test
+    void lengthShouldReturnZeroOnAllZeroInput() {
+        Line line = new Line(0, 0, 0, 0);
+        double length = line.length();
+        assertEquals(0, length);
+    }
+}
