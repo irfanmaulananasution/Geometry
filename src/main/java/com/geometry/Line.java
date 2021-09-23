474d712e0bd3f93ae4a6500913bc49ffda180fd7
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2021-09-23 11:06:58 +0700

    Add length behaviour : same x axis and first y less than second y return correct length

--- src/main/java/com/geometry/Line.java
+++ src/main/java/com/geometry/Line.java
@@ -1,11 +1,15 @@
 package com.geometry;
 
 public class Line {
+    private final int y1;
+    private final int y2;
 
     public Line(int x1, int y1, int x2, int y2) {
+        this.y1 = y1;
+        this.y2 = y2;
     }
 
     public double getLength() {
-        return 0;
+        return y2 - y1;
     }
 }
