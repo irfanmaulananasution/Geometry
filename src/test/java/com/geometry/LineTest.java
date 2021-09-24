8f1dcccef9022069e7613ed0af5bbe032fe23ec1
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2021-09-24 08:04:57 +0700

    Add length behaviour : same x axis and first y less than second y return correct length

--- src/test/java/com/geometry/LineTest.java
+++ src/test/java/com/geometry/LineTest.java
@@ -5,11 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LineTest {
+    private static final double EPS = 1e-9;
 
     @Test
     void coincidePointsShouldReturnZeroLength() {
         Line line = new Line(0, 0, 0, 0);
         double length = line.getLength();
-        assertEquals(0, length, 1e-9);
+        assertEquals(0, length, EPS);
+    }
+
+    @Test
+    void sameXAxisAndFirstYLessThanSecondYShouldReturnCorrectLength() {
+        Line line = new Line(0, 1, 0, 2);
+        double length = line.getLength();
+        assertEquals(1, length, EPS);
     }
 }
